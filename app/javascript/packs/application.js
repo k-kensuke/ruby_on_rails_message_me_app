20912e5e14a72c0e8d142d518ddabb4720124171
ken <aq150808@gmail.com>
2020-06-08 06:26:42 +0000

    200608-1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,26 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("semantic-ui-sass")
 
 
+// import $ from 'jquery';
+// window.jQuery = $;
+// window.$ = $;
+
+// $(document).on('turbolinks:load', function(){
+//     $('.ui.dropdown').dropdown();
+// })
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown())
